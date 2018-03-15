2125f74b3328d80c2cbc321a29dc19b8b963a88f
James Perry Johnson <Perry.Johnson@vml.com>
2018-03-15 01:17:42 -0500

    Comment code cleanup & updating

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# project1
+Music Concierge
+#
+Product Description: A website to help users receive upcoming concerts, biographies and videos of top artists
+
+Usage
+#
+Enter artist name in search bar. It will return an artist card, complete with a biography, link to upcoming concerts and their top-viewed vide.
+
+Built With
+#
+LastFM API
+YouTube Data API
+BandsInTown API
+
+Contributors
+ * James Johnson - https://www.linkedin.com/in/jamesperryjohnson/
+ * Cameron Wilkinson - https://www.linkedin.com/in/cameron-wilkinson-b4936614b/
+ * Jennifer Lang - https://www.linkedin.com/in/jennifer-lang-5017aa126/
+ * Patrick Shehan - https://www.linkedin.com/in/patrick-shehan/
+
+
+License
+#
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,5 @@
 "use strict;"
 
-/*
-var someElement = $('<a class="dummy-media-object" href="https://twitter.com/SaraSoueidan"><img class="round" src="https://lastfm-img2.akamaized.net/i/u/174s/3bace338cca24495c44dbbfed0e8fd4f.png" alt="Sara Soueidan"/><h3>Kendrick Lamar</h3></a>');
-*/
-
 var apiKeyOne = "3ab16c26dfaba69c000d8a6d7542d205";
 var apiKeyTwo = "AIzaSyCsKTmPxVHUp9o0kXMu5_gkI2XQU1ohWIA";
 var toplistsShowing = false;
@@ -11,7 +7,6 @@ var searchListShowing = false;
 
 function incrementResultCounter() {
     resultCounter++;
-    console.log(resultCounter);
 }
 
 var resultCounter = {
@@ -20,7 +15,6 @@ var resultCounter = {
 
 function displayArtistInfo(data, location) {
     var counter = resultCounter.num;
-    console.log(counter);
     var searchListShowing = true;
     var itemArtist = data.artist.name;
     var itemBio = data.artist.bio.summary;
@@ -31,51 +25,37 @@ function displayArtistInfo(data, location) {
     videoSearchQuery.concat(queryAppend);
 
     var videoLocation = '#artist-video' + counter;
-    //console.log(itemArtist);
-    //console.log(itemBio);
-    //console.log(itemImage);
-
-    /*
-    var newListItem = $('<a class="dummy-media-object artist-result-tile" href="#" value="' + itemArtist.trim() + '"><div><img class="round" src="' + itemImage + '" alt="' + itemArtist + '"/><h3>' + itemArtist + '</h3><p>' + itemBio + '</p><div></a>');
-    */
-
-    /*
-    var newListItem = $('<a class="dummy-media-object artist-result-tile" href="#" value="' + itemArtist.trim() + '"><img class="round" src="' + itemImage + '" alt="' + itemArtist + '"/><h3>' + itemArtist + '</h3>' + '<span>' + itemBio + '</span></a>');
-    */
     var newEventWidget = $('<div class="card mb-4 bg-primary text-white"><div class="card-body"><script charset="utf-8" src="https://widget.bandsintown.com/main.min.js"></script><a class="card-text bit-widget-initializer" data-artist-name="' + itemArtist + '" data-display-local-dates="false" data-display-past-dates="true" data-auto-style="false" data-text-color="#000000" data-link-color="#2F95DE" data-popup-background-color="#FFFFFF" data-background-color="#FFFFFF" data-display-limit="15" data-link-text-color="#FFFFFF"></a><h5 </div>');
-    //console.log(newEventWidget);
-
     var newCard = $('<div class="row"><div class="card dummy-media-object artist-result"><div class="card-body"><li class="media"><img class="mr-3" src="' + itemImage + '" alt="Generic placeholder image"><div class="media-body"><h5 class="mt-0 mb-1">' + itemArtist + '</h5><p class="text-left">' + itemBio + '</p><div id="artist-video' + counter + '">VIDEO</div></div></li><div class="card text-center border-0"><div class="mb-2" id="accordion' + counter + '"><button class="btn btn-link p-0" data-toggle="collapse" data-target="#collapse' + counter + '" aria-expanded="true" aria-controls="collapse' + counter + '">Click here for concert schedule</button></div></div><div id="collapse' + counter + '" class="collapse" aria-labelledby="headingOne" data-parent="#accordion' + counter + '"><div class="card-body" id="collapse-widget">' + newEventWidget["0"].innerHTML + '</div></div></div></div></div>');
-
 
     $(location).append(newCard);
     displayArtistVideo(videoSearchQuery, videoLocation);
     counter++;
     resultCounter.num = counter;
-    //console.log(resultCounter.num);
 }
 
 function displayArtistVideo(query, location) {
-    console.log('video search query: ', query);
-    console.log('video location: ', location);
 
-    function tplawesome(e, t) { res = e; for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function (e, r) { return t[n][r] }) } return res }
+    function tplawesome(e, t) {
+        res = e;
+        for (var n = 0; n < t.length; n++) {
+            res = res.replace(/\{\{(.*?)\}\}/g, function (e, r) {
+                return t[n][r]
+            })
+        }
+        return res
+    }
 
     // prepare the request
     var request = gapi.client.youtube.search.list({
         part: "snippet",
         type: "video",
         q: query,
-        //q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
         maxResults: 1,
         order: "relevance",
         autoplay: 1,
         loop: 1
     });
-
-    console.log('request: ', request);
-    console.log('video location: ', location);
-    //console.log('request.q: ', request.q);
 
     // execute the request
     request.execute(function (response) {
@@ -83,7 +63,7 @@ function displayArtistVideo(query, location) {
         console.log(results);
         $(location).html("");
         $.each(results.items, function (index, item) {
-            $.get("tpl/item.html", function (data) {
+            $.get("assets/tpl/item.html", function (data) {
                 $(location).append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoId }]));
             });
         });
@@ -92,7 +72,6 @@ function displayArtistVideo(query, location) {
 }
 
 function displayTopArtistList(inputObj, location) {
-
     for (var i = 0; i < inputObj.artists.artist.length; i++) {
         var itemArtist = inputObj.artists.artist[i].name;
         var itemImage = inputObj.artists.artist[i].image[3]["#text"];
@@ -114,20 +93,7 @@ function displayTopTrackPanel(inputObj, location) {
 }
 
 function displayTopAlbumPerTag(inputObj, location) {
-    //console.log(inputObj.length);
-    /*
-    console.log(inputObj);
-    console.log(inputObj.length);
-    console.log(inputObj[0][0][0]);
-    console.log(inputObj[1][0][1]);
-    console.log(inputObj[2][0]);
-    console.log(inputObj[3][0]);
-    console.log(inputObj[1]);
-    */
-    //console.log(inputObj[0]['0']);
-
     for (var i = 0; i < inputObj.length; i++) {
-        //console.log(inputObj[i][0][0]);
         var itemGenre = inputObj[i][0][0];
         var itemAlbum = inputObj[i][0][1];
         var itemArtist = inputObj[i][0][2];
@@ -138,20 +104,13 @@ function displayTopAlbumPerTag(inputObj, location) {
     }
 }
 
-function retrieveArtistEvents() {
-    //stub
-}
-
 function retrieveArtistInfo(query) {
-
     var queryURL = 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + query + '&api_key=' + apiKeyOne + '&format=json&limit=10';
-    //console.log(queryURL);
 
     $.ajax({
         method: "GET",
         url: queryURL
     }).then(function (response) {
-        //console.log(response);
         displayArtistInfo(response, '.morphsearch-results');
     });
 }
@@ -181,8 +140,6 @@ function retrieveTopArtists() {
 function retrieveTopTags() {
     var queryURL = 'https://ws.audioscrobbler.com/2.0/?method=chart.gettoptags&api_key=' + apiKeyOne + '&format=json&limit=10';
     var tagListArr = [];
-    //console.log(queryURL);
-    //console.log(tagListArr);
 
     $.ajax({
         method: "GET",
@@ -190,51 +147,41 @@ function retrieveTopTags() {
     }).then(function (response) {
         for (var i = 0; i < response.tags.tag.length; i++) {
             tagListArr.push(response.tags.tag[i].name);
-            //console.log(tagListArr);
         }
         retrieveTopAlbumPerTag(tagListArr);
     });
 }
 
 function retrieveTopAlbumPerTag(inputObj) {
-    //console.log(inputObj);
     var albumListArr = [];
 
     function addWorkArrItem(item) {
         albumListArr.push(item);
-        //console.log(workArr.length);
     }
 
     for (var i = 0; i < inputObj.length; i++) {
         var index = i;
         var queryURL = 'https://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=' + inputObj[index] + '&api_key=' + apiKeyOne + '&format=json&limit=1';
-        //console.log(index, inputObj[index]);
 
         $.ajax({
             method: "GET",
             url: queryURL
         }).then(function (response) {
-            /*
-            console.log(response);
-            console.log('Genre: ', response.albums['@attr']['tag']);
-            console.log('Album name: ', response.albums.album[0].name);
-            console.log('Artist name: ', response.albums.album[0].artist.name);
-            console.log('Image source: ', response.albums.album[0].image[3]["#text"]);
-            */
+
             var tempArr = [];
             var genre = (response.albums['@attr']['tag']);
             var album = (response.albums.album[0].name);
             var artist = (response.albums.album[0].artist.name);
             var image = (response.albums.album[0].image[3]["#text"]);
-            //console.log(index, genre);
+
             tempArr.push([genre, album, artist, image]);
             addWorkArrItem(tempArr);
-            //console.log(albumListArr.length);
+
             if (albumListArr.length === 10) {
                 displayTopAlbumPerTag(albumListArr, '#search-panel-right');
             }
-
         });
+
         index++;
     }
 
@@ -256,15 +203,9 @@ $(document).on('click', '.artist-tile', function (ev) {
 
     rawQuery = $(this).attr('value').trim();
     searchQuery = rawQuery.replace(/\s/g, '+');
-    //console.log('raw query: ', rawQuery);
-    //console.log('search query: ', searchQuery);
 
     $("#searchQuery").attr('value', rawQuery);
-
     $(".morphsearch-content").hide();
-    /*
-    $(".morphsearch-results").append('<a class="dummy-media-object" href="https://twitter.com/SaraSoueidan"><img class="round" src="https://lastfm-img2.akamaized.net/i/u/174s/3bace338cca24495c44dbbfed0e8fd4f.png" alt="Sara Soueidan"/><h3>' + document.getElementById("searchQuery").value + '</h3></a>');
-    */
 
     retrieveArtistInfo(searchQuery);
 });
@@ -274,12 +215,15 @@ $(document).on('click', '.artist-tile', function (ev) {
         input = morphSearch.querySelector('input.morphsearch-input'),
         ctrlClose = morphSearch.querySelector('span.morphsearch-close'),
         isOpen = isAnimating = false,
+
         // show/hide search area
         toggleSearch = function (evt) {
+
             // return if open and the input gets focused
             if (evt.type.toLowerCase() === 'focus' && isOpen) return false;
 
             var offsets = morphsearch.getBoundingClientRect();
+
             if (isOpen) {
                 classie.remove(morphSearch, 'open');
 
@@ -296,20 +240,22 @@ $(document).on('click', '.artist-tile', function (ev) {
                 }
 
                 input.blur();
-            }
-            else {
+            } else {
                 classie.add(morphSearch, 'open');
             }
+
             isOpen = !isOpen;
         };
 
     // events
     input.addEventListener('focus', toggleSearch);
     ctrlClose.addEventListener('click', toggleSearch);
+
     // esc key closes search overlay
     // keyboard navigation events
     document.addEventListener('keydown', function (ev) {
         var keyCode = ev.keyCode || ev.which;
+
         if (keyCode === 27 && isOpen) {
             toggleSearch(ev);
             $(".morphsearch-content").show();
@@ -317,20 +263,14 @@ $(document).on('click', '.artist-tile', function (ev) {
         }
     });
 
-
-
-    /***** for demo purposes only: don't allow to submit the form *****/
     morphSearch.querySelector('button[type="submit"]').addEventListener('click', function (ev) {
         ev.preventDefault();
+
         if ((document.getElementById("searchQuery").value).trim()) {
             $(".morphsearch-content").hide();
-            //console.log(document.getElementById("searchQuery").value);
 
             var rawQuery = document.getElementById("searchQuery").value.trim();
             searchQuery = rawQuery.replace(/\s/g, '+');
-
-            //console.log('raw query: ', rawQuery);
-            //console.log('search query: ', searchQuery);
 
             retrieveArtistInfo(searchQuery, '.morphsearch-results');
         }
@@ -338,12 +278,11 @@ $(document).on('click', '.artist-tile', function (ev) {
 
     morphSearch.querySelector('span[class="morphsearch-close"]').addEventListener('click', function (ev) {
         ev.preventDefault();
+
         $(".morphsearch-content").show();
         $(".morphsearch-results").empty();
     });
 })();
-
-// Youtube video code
 
 function resetVideoHeight(location) {
     $(".video").css("height", $(location).width() * 9 / 16);
@@ -352,44 +291,6 @@ function resetVideoHeight(location) {
 function init() {
     gapi.client.setApiKey(apiKeyTwo);
     gapi.client.load("youtube", "v3", function () {
-        // yt api is ready
-        //retrieveArtistInfo();
+        // YouTube API is ready
     });
-
-    console.log(gapi);
 }
-
-/*
-$(function () {
-    $("form").on("submit", function (e) {
-        e.preventDefault();
-        // prepare the request
-        var request = gapi.client.youtube.search.list({
-            part: "snippet",
-            type: "video",
-            q: encodeURIComponent($("#search").val()),
-            maxResults: 1,
-            order: "viewCount",
-            autoplay: 1,
-            loop: 1,
-        });
-        // execute the request
-        request.execute(function (response) {
-            var results = response.result;
-            $("#artist-video").html("");
-            $.each(results.items, function (index, item) {
-                $.get("yt.html", function (data) {
-                    $("#artist-video").append(yt(data, [
-                        {
-                            "title": item.snippet.title,
-                            "videoid": item.id.videoId
-                        }]));
-                });
-            });
-            resetVideoHeight();
-        });
-    });
-
-    $(window).on("resize", resetVideoHeight);
-});
-*/
